b8954184ed776cb58c18c4a3c7ec562fb98eef31
jbecker3 <5340771+jbecker3@users.noreply.github.com>
2024-02-08 16:51:19 -0500

    Added error checking for json file input

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <nlohmann/json.hpp>
 
+#define CHECK_JSON(a,b) a.contains(b)
 
 int main(int argc, char* argv[])
 {
@@ -50,24 +51,35 @@ int main(int argc, char* argv[])
             }
 
             IQ_Parameters parameters{};
-            parameters.center_freq = stream_data["center_freq"];
-            parameters.sample_rate = stream_data["sample_rate"];
-            parameters.iq_bandwidth = stream_data["iq_bandwidth"];
-            parameters.ref_level = stream_data["ref_level"];
-            parameters.stream_id = stream_data["stream_id"];
-            parameters.packet_size = stream_data["packet_size"];
-            int packet_count = stream_data["packet_count"];
-            std::string output_file = stream_data["output_file"];
-
-            double sample_rate;
-            if (connection_type == "net")
+            int packet_count = 0;
+            std::string output_filename;
+            if(stream_data.contains("center_freq") &&
+               stream_data.contains("sample_rate") &&
+               stream_data.contains("iq_bandwidth") &&
+               stream_data.contains("ref_level") &&
+               stream_data.contains("stream_id") &&
+               stream_data.contains("packet_size") &&
+               stream_data.contains("packet_count") &&
+               stream_data.contains("output_file"))
             {
-                sample_rate = 50.0 /float(parameters.sample_rate);
+                parameters.center_freq = stream_data["center_freq"];
+                parameters.sample_rate = stream_data["sample_rate"];
+                parameters.iq_bandwidth = stream_data["iq_bandwidth"];
+                parameters.ref_level = stream_data["ref_level"];
+                parameters.stream_id = stream_data["stream_id"];
+                parameters.packet_size = stream_data["packet_size"];
+                packet_count = stream_data["packet_count"];
+                output_filename = stream_data["output_file"];
             }
             else
             {
-                sample_rate = 200.0 /float(parameters.sample_rate);
+                std::cerr << "Input file does not contain all necessary fields." << std::endl;
+                return 1;
             }
+
+            double sample_rate;
+            if (connection_type == "net") sample_rate = 50.0 /float(parameters.sample_rate);
+            else sample_rate = 200.0 /float(parameters.sample_rate);
 
             std::cout << "Recording IQ Stream with these Parameters:" << std::endl;
             std::cout << "\tCenter Frequency: " << parameters.center_freq / 1.0e6 << "MHz" << std::endl;
@@ -77,10 +89,10 @@ int main(int argc, char* argv[])
             std::cout << "\tStream ID: " << parameters.stream_id << std::endl;
             std::cout << "\tPacket Size: " << parameters.packet_size << " words" << std::endl;
             std::cout << "\tPacket Count: " << packet_count << " packets" << std::endl;
-            std::cout << "\tOutput File: " << output_file << std::endl;
+            std::cout << "\tOutput File: " << output_filename << std::endl;
 
             if (connection_type != "test") stream.getVRTPackets(packet_count,parameters);
-            stream.writeStream(output_file);
+            stream.writeStream(output_filename);
         }
     }
     else
